b4e587f54daf295233a04feeb175e5bba795fb8b
Fara <119137860+fara18@users.noreply.github.com>
2023-03-21 21:32:35 +0100

    homework commit

--- Homework/19.03.2023/script.js
+++ Homework/19.03.2023/script.js
@@ -9,10 +9,10 @@ const getHighest = (num1, num2) => {
 }
 
 // Напишите функцию, которая принимает число и возвращает его квадрат.
-const getSquared = (num) => Math.pow(num, 2);
+const getSquared = num => Math.pow(num, 2);
 
 // Напишите функцию, которая принимает строку и возвращает ее длину.
-const getLength = (string) => string.length
+const getLength = string => string.length
 
 // Напишите функцию, которая принимает два числа и возвращает их сумму.
 const getSum = (num1, num2) => num1 + num2
@@ -21,18 +21,18 @@ const getSum = (num1, num2) => num1 + num2
 let array = [1, 2, 3, 4, 5]
 let newArray = []
 
-const getSquaredArray = (array) => {
+const getSquaredArray = array => {
     for (let i = 0; i < array.length; i++) {
-        newArray.push(Math.pow(array[i], 2))
+        newArray.push(array[i] * 2)
     } return newArray
 }
 
 // Напишите функцию, которая принимает строку и возвращает новую строку, в которой все гласные заменены на символ '*'.
-const replaceVowel = (string) => string.replace(/[aeiou]/gi, '*');
+const replaceVowel = string => string.replace(/[aeiou]/gi, '*');
 
 // Напишите функцию, которая принимает массив чисел и возвращает среднее значение.
 let average = 0
-const getAverage = (array) => {
+const getAverage = array => {
 
     for (let i = 0; i < array.length; i++) {
         average += array[i]
---
+++ Homework/20.03.2023 /script.js
@@ -0,0 +1,50 @@
+// ДЗ (for и forEach)
+
+// 1. Дан массив с числами. Вычислить сумму элементов массива
+
+/* Using forLoop */
+const array = [5, 5];
+let arraySum = 0;
+
+for (i = 0; i < array.length; i++) {
+    arraySum += array[i];
+}
+/* Using forEach */
+array.forEach(sum => arraySum += sum);
+
+// 2. Дан массив со строками. Сформировать новый массив, в который попадут все строки из исходного массива, длина которых больше 7 символов
+
+const strArr = ["Hi", "Hello i'm Faruh", "1234567"];
+let newArr = [];
+
+/* Using forLoop */
+for (i = 0; i < strArr.length; i++) {
+    strArr[i].length >= 7 ? newArr.push(strArr[i]) : '';
+}
+/* Using forEach */
+strArr.forEach(str => str.length >= 7 ? newArr.push(str) : '')
+
+// 3. Дан массив со строками. Сформировать новый массив, в который попадут все строки из исходного массива, которые начинаются на букву D или d
+
+// 4. Дан массив с числами. Сформировать новый массив, в который попадут все числа из исходного массива, возведенные в 3 степень
+
+let powArr = [];
+
+/* Using forLoop */
+for (let i = 0; i < array.length; i++) {
+    powArr.push(Math.pow(array[i], 3))
+}
+/* Using forEach */
+array.forEach(index => powArr.push(Math.pow(index, 3)))
+
+// 5. Дан массив с числами. Сформировать новый массив, в который попадут все числа из исходного массива, заканчивающиеся на 7
+const arr = [27, 17, 9, 78, 7, 0, 9, 2];
+let arrayWith7 = [];
+
+/* Using forLoop */
+for (i = 0; i < arr.length; i++) {
+    arr[i] % 10 === 7 ? arrayWith7.push(arr[i]) : ''
+}
+/* Using forEach */
+arr.forEach(index => index % 10 === 7 ? arrayWith7.push(index) : '');
+
